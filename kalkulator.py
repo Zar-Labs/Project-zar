9fd64175809fa4c8ea22919c705b8977c228ea19
Zar-Labs <nizarzuhdyalkautsar@gmail.com>
2026-01-13 04:21:10 +0000

    Nambahin file kalkulator yang ketinggalan

---
+++ kalkulator.py
@@ -0,0 +1,10 @@
+print("=== KALKULATOR CANGGIH ZAR ===")
+
+a = int(input("Masukan angka pertama: "))
+b = int(input("Masukan angka kedua: "))
+
+tambah = a + b
+kurang = a - b
+
+print(f"Hasil  {a} + {b} adalah: {tambah}")
+print(f"Hasil  {a} - {b} adalah: {kurang}")
